6c617279bf2e71f5aaf5f5f1fcd6c3683701d861
Vincent Walkergit config --global user.email vincethewalker@gmail.com <vincethewalker@yahoo.co.uk>
2017-11-23 11:29:49 +0000

    created a filter to return to the root page as well as css styles

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :select_plan, only: :new
     
     def create
         super do |resource|
@@ -11,5 +12,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
             end
           end
        end
+    end
+    
+  private
+    def select_plan
+      unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+      flash[:notice] = "Please choose a membership plan to sign up."
+      redirect_to root_url
+      end
     end
 end
