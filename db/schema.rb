a7af7d4a8f8e03f64107f6788c7c771578264906
Vincent Walkergit config --global user.email vincethewalker@gmail.com <vincethewalker@yahoo.co.uk>
2017-11-24 15:23:25 +0000

    re-added profile model and table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124080101) do
+ActiveRecord::Schema.define(version: 20171124152059) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 20171124080101) do
   end
 
   create_table "profiles", force: true do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "job_title"
+    t.string   "phone_number"
+    t.string   "contact_email"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
