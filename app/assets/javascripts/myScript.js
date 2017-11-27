e0893f68f6cba946e7149a2ad017d6d0605e5aa0
Vincent Walkergit config --global user.email vincethewalker@gmail.com <vincethewalker@yahoo.co.uk>
2017-11-27 13:31:49 +0000

    General Home improvements, Created user name in nav bar and some js to create btn change

---
+++ app/assets/javascripts/myScript.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#orange').mouseenter(function(){
+        $('#orange').css({"background-color": "#E8A558", "color": "#ffffff"})
+    });
+   $('#orange').mouseleave(function(){
+        $('#orange').css("background-color", "#FFA500")	
+   });
+});
